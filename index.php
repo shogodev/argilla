40d3e4ba2b8fcbd889ed17d72088b328ccf9ca0c
glagolev <glagolev@shogo.ru>
2013-03-06 14:30:16 +0400

    Bug #30: Добавил вывод ошибок в debug режиме

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ $production = dirname(__FILE__).'/protected/config/production.php';
 if( file_exists($production) )
 {
   error_reporting(0);
+  ini_set('display_errors', 0);
   $config = require_once $production;
 }
 else
 {
   error_reporting(E_ALL);
+  ini_set('display_errors', 1);
   $config = require_once dirname(__FILE__).'/protected/config/frontend.php';
 }
 
