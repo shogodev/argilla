1c63b25e6282ebc58332817b19ff53c2d9ebe1f0
utenkov <utenkov@shogo.ru>
2013-04-03 11:57:49 +0300

    Enh: Дабавить тесты для модели продукта

--- backend/protected/tests/fixtures/{{product_assignment}}.php
+++ backend/protected/tests/fixtures/{{product_assignment}}.php
@@ -5,7 +5,18 @@
  * @copyright Copyright &copy; 2003-2013 Shogo
  * @license http://argilla.ru/LICENSE
  */
-return array(
-  'asgn1' => array('product_id' => 1, 'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1),
-  'asgn2' => array('product_id' => 2, 'section_id' => 4, 'type_id' => 4, 'category_id' => 1, 'collection_id' => 1),
-);
+return [
+
+  'assignment1'  => ['product_id' => 1,  'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment2'  => ['product_id' => 2,  'section_id' => 4, 'type_id' => 4, 'category_id' => 1, 'collection_id' => 1],
+  'assignment3'  => ['product_id' => 2,  'section_id' => 3, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment4'  => ['product_id' => 3,  'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment5'  => ['product_id' => 4,  'section_id' => 2, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment6'  => ['product_id' => 5,  'section_id' => 3, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment7'  => ['product_id' => 6,  'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment8'  => ['product_id' => 7,  'section_id' => 2, 'type_id' => 2, 'category_id' => 1, 'collection_id' => 1],
+  'assignment9'  => ['product_id' => 8,  'section_id' => 2, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment10' => ['product_id' => 9,  'section_id' => 3, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
+  'assignment11' => ['product_id' => 10, 'section_id' => 1, 'type_id' => 2, 'category_id' => 1, 'collection_id' => 1],
+
+];
