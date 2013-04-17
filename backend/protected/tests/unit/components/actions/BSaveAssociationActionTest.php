4fa8c35015f58957805cb68ef0e5cf88ff16dac7
glagolev <glagolev@shogo.ru>
2013-04-17 10:28:07 +0400

    Покрыт тестами BSaveAssociationAction

---
+++ backend/protected/tests/unit/components/actions/BSaveAssociationActionTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * @author Sergey Glagolev <glagolev@shogo.ru>
+ * @link https://github.com/shogodev/argilla/
+ * @copyright Copyright &copy; 2003-2013 Shogo
+ * @license http://argilla.ru/LICENSE
+ */
+class BSaveAssociationActionTest extends CDbTestCase
+{
+  public $fixtures = array(
+    'product' => 'BProduct',
+  );
+
+  public function setUp()
+  {
+    Yii::app()->setUnitEnvironment('product', 'BProduct');
+    Yii::app()->setAjaxRequest();
+
+    parent::setUp();
+  }
+
+  public function testRunOne()
+  {
+    $this->getFixtureManager()->truncateTable('{{association}}');
+    $model = Bproduct::model()->findByPk(11);
+    $this->assertEmpty($model->associations);
+
+    $action = new BSaveAssociationAction(Yii::app()->controller, 'saveAssociations');
+
+    $_POST['ids']   = 12;
+    $_POST['value'] = 1;
+    $action->run('BProduct', 11, 'BProduct');
+
+    $this->assertEquals(12, Arr::reset($model->getRelated('associations', true))->dst_id);
+  }
+
+  public function testRunMany()
+  {
+    $this->getFixtureManager()->truncateTable('{{association}}');
+    $action = new BSaveAssociationAction(Yii::app()->controller, 'saveAssociations');
+
+    $_POST['ids']   = array(12, 13);
+    $_POST['value'] = 1;
+    $action->run('BProduct', 11, 'BProduct');
+
+    $model = Bproduct::model()->findByPk(11);
+    $this->assertEquals(12, Arr::reset($model->associations)->dst_id);
+    $this->assertEquals(13, Arr::end($model->associations)->dst_id);
+  }
+
+  public function testRunDelete()
+  {
+    $this->getFixtureManager()->truncateTable('{{association}}');
+    $action = new BSaveAssociationAction(Yii::app()->controller, 'saveAssociations');
+
+    $_POST['ids']   = 12;
+    $_POST['value'] = 1;
+    $action->run('BProduct', 11, 'BProduct');
+
+    $model = Bproduct::model()->findByPk(11);
+    $this->assertEquals(12, Arr::reset($model->associations)->dst_id);
+
+    $_POST['ids']   = 12;
+    $_POST['value'] = 0;
+    $action->run('BProduct', 11, 'BProduct');
+
+    $this->assertEmpty($model->getRelated('associations', true));
+  }
+}
