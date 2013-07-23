0a7358182597eb208035bb5caf3578da37ca585d
utenkov <utenkov@shogo.ru>
2013-07-23 11:07:16 +0300

    Enh: Изменить тесты для BProductTest

--- backend/protected/tests/fixtures/{{product_assignment}}.php
+++ backend/protected/tests/fixtures/{{product_assignment}}.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * @author Sergey Glagolev <glagolev@shogo.ru>
- * @link https://github.com/shogodev/argilla/
+ * @author    Sergey Glagolev <glagolev@shogo.ru>
+ * @link      https://github.com/shogodev/argilla/
  * @copyright Copyright &copy; 2003-2013 Shogo
- * @license http://argilla.ru/LICENSE
+ * @license   http://argilla.ru/LICENSE
  */
 return [
 
-  'assignment1'  => ['product_id' => 1,  'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment2'  => ['product_id' => 2,  'section_id' => 4, 'type_id' => 4, 'category_id' => 1, 'collection_id' => 1],
-  'assignment3'  => ['product_id' => 2,  'section_id' => 3, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment4'  => ['product_id' => 3,  'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment5'  => ['product_id' => 4,  'section_id' => 2, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment6'  => ['product_id' => 5,  'section_id' => 3, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment7'  => ['product_id' => 6,  'section_id' => 1, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment8'  => ['product_id' => 7,  'section_id' => 2, 'type_id' => 2, 'category_id' => 1, 'collection_id' => 1],
-  'assignment9'  => ['product_id' => 8,  'section_id' => 2, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment10' => ['product_id' => 9,  'section_id' => 3, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1],
-  'assignment11' => ['product_id' => 10, 'section_id' => 1, 'type_id' => 2, 'category_id' => 1, 'collection_id' => 1],
+  'assignment1' => array('product_id' => 1, 'section_id' => 1, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment2' => array('product_id' => 2, 'section_id' => 4, 'type_id' => 4, 'category_id' => 1, 'collection_id' => 1),
+  'assignment3' => array('product_id' => 2, 'section_id' => 3, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment4' => array('product_id' => 3, 'section_id' => 1, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment5' => array('product_id' => 4, 'section_id' => 2, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment6' => array('product_id' => 5, 'section_id' => 3, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment7' => array('product_id' => 6, 'section_id' => 1, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment8' => array('product_id' => 7, 'section_id' => 2, 'type_id' => 2, 'category_id' => 1, 'collection_id' => 1),
+  'assignment9' => array('product_id' => 8, 'section_id' => 2, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment10' => array('product_id' => 9, 'section_id' => 3, 'type_id' => 42, 'category_id' => 1, 'collection_id' => 1),
+  'assignment11' => array('product_id' => 10, 'section_id' => 1, 'type_id' => 2, 'category_id' => 1, 'collection_id' => 1),
 
   'asgn2' => array('product_id' => 14, 'section_id' => 333, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1),
   'asgn3' => array('product_id' => 15, 'section_id' => 444, 'type_id' => 1, 'category_id' => 1, 'collection_id' => 1),
