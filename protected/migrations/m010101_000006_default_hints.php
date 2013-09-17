141812c6f040da988c23dd83fefe8f5f5e45692d
glagolev <glagolev@shogo.ru>
2013-09-17 14:08:09 +0400

    Enh: В бэкенд добавлены подсказки для формата задания редиректов

--- protected/migrations/m010101_000006_default_hints.php
+++ protected/migrations/m010101_000006_default_hints.php
@@ -5,7 +5,10 @@ class m010101_000006_default_hints extends CDbMigration
 	public function up()
 	{
       $this->execute("INSERT INTO `".$this->dbConnection->tablePrefix."hint` (`id`, `model`, `attribute`, `content`, `popup`) VALUES
-                      (1, 'BMetaRoute', 'title', 'Для приведения переменных к регистру доступны функции:<br/><b>ucfirst{переменная}</b> - возводит первую букву в верхний регистр; <br/><b>upper{переменная}</b> - возводит в верхний регистр; <br/><b>lower{переменная}</b> - приводит к нижниму регистру; ', 0)");
+                      (1, 'BMetaRoute', 'title', 'Для приведения переменных к регистру доступны функции:<br/><b>ucfirst{переменная}</b> - возводит первую букву в верхний регистр; <br/><b>upper{переменная}</b> - возводит в верхний регистр; <br/><b>lower{переменная}</b> - приводит к нижниму регистру; ', 0),
+                      (2, 'BRedirect', 'base', 'Можно задавать регулярное выражение. Формат: <b>#/komplekty/(\\d+)#</b>', 0),
+                      (3, 'BRedirect', 'target', 'Можно получить параметры из регулярного выражения. Формат: <b>/aksessuary_dlya_lyzh/$1</b>', 0);
+                      ");
 	}
 
 	public function down()
