7361690fa394868d4a7aeaf6b04468adb1e87f61
tatarinov <tatarinov@shogo.ru>
2014-05-21 16:56:21 +0400

    Enh: Добавилась возможность использования подмены переменных в текстовых блоках

--- protected/share/behaviors/CommonBehavior.php
+++ protected/share/behaviors/CommonBehavior.php
@@ -9,11 +9,6 @@
  */
 class CommonBehavior extends CBehavior
 {
-  /**
-   * @var TextBlock[] $textBlocks
-   */
-  protected $textBlocks = array();
-
   private $contacts;
 
   private $settings;
@@ -22,81 +17,8 @@ class CommonBehavior extends CBehavior
   {
     parent::attach($owner);
 
-    Yii::import('frontend.components.ar.FActiveRecord');
-    Yii::import('frontend.components.image.FSingleImage');
-    Yii::import('frontend.components.image.ImageInterface');
-
-    Yii::import('frontend.models.TextBlock');
     Yii::import('frontend.models.contact.*');
     Yii::import('frontend.models.Settings');
-  }
-
-
-  /**
-   * @param $key
-   *
-   * @return TextBlock
-   */
-  public function textBlock($key)
-  {
-    return Arr::reduce($this->textBlocks($key));
-  }
-
-  /**
-   * @param $key
-   *
-   * @return TextBlock[]
-   */
-  public function textBlocks($key)
-  {
-    if( !isset($this->textBlocks[$key]) )
-      $this->textBlocks[$key] = TextBlock::model()->findAllByAttributes(array('location' => $key));
-
-    return $this->textBlocks[$key];
-  }
-
-  /**
-   * Если текстовый блок не существует, то он создается
-   * @param string|null $name - название
-   * @param string|null $content - текст по умолчанию
-   * @param array('class' => 'success-message') $htmlOptions
-   * @return string
-   */
-  public function textBlockRegister($name = null, $content = null, $htmlOptions = array('class' => 'success-message'))
-  {
-    if( $name === null )
-      $location = $this->owner->route;
-    else
-      $location = Utils::translite($name);
-
-    if( !$this->textBlock($location) )
-    {
-      $textBlock = new TextBlock();
-      $textBlock->attributes = array(
-        'location' => $location,
-        'name' => empty($name) ? $this->owner->route : $name.' ('.$this->owner->route.')',
-        'visible' => 1,
-        'auto_created' => 1
-      );
-
-      if( $content !== null )
-        $textBlock->setAttribute('content', $htmlOptions != null ? CHtml::tag('div', $htmlOptions, $content) : $content);
-
-      $textBlock->save();
-
-      if( $content === null )
-      {
-        $textBlock->content = '<div style="background-color: red; padding: 10px; color: white; font-size: 12px; font-weight: bold">';
-        $textBlock->content .= 'Данный текстовый блок сгенерирован автоматически в '.$this->owner->route.'.';
-        $textBlock->content .= 'Текст нужно заменить в <a href="/backend/textblock/bTextBlock/update/'.$textBlock->id.'">backend</a>';
-        $textBlock->content .= '</div>';
-        $textBlock->save();
-      }
-
-      $this->textBlocks[$location] = $textBlock;
-    }
-
-    return $this->textBlock($location)->content;
   }
 
   public function getContacts()
--- protected/tests/fixtures/{{text_block}}.php
+++ protected/tests/fixtures/{{text_block}}.php
@@ -4,4 +4,8 @@ return array(
   '2' => array('id' => '2', 'location' => 'main', 'name' => 'Блок на главной 2', 'content' => 'Текст 2',  'visible' => 1, 'position' => 20),
   '3' => array('id' => '3', 'location' => 'main', 'name' => 'Блок на главной 3', 'content' => 'Текст 3',  'visible' => 0, 'position' => 10),
   '4' => array('id' => '4', 'location' => 'mainNotVisible', 'name' => 'Тест2', 'content' => 'testNotVisible', 'visible' => 0),
+  '5' => array('id' => '5', 'location' => 'testReplace', 'name' => 'Test replace', 'content' => 'text {before}', 'visible' => 1),
+  '6' => array('id' => '6', 'location' => 'testReplaceByTextBlock', 'name' => 'Test replace text block', 'content' => 'text2 {replaced_text_block}', 'visible' => 1),
+  '7' => array('id' => '7', 'location' => 'replaced_text_block', 'name' => 'Test replace text block', 'content' => 'new replaced text', 'visible' => 1),
+  '8' => array('id' => '8', 'location' => 'test_replace_registration', 'name' => 'Test replace', 'content' => 'text3 {before}', 'visible' => 1),
 );
