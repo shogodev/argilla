2b2b7ec1ace635320edc5c84fd8ce4178dc7b11f
glagolev <glagolev@shogo.ru>
2014-06-10 17:28:42 +0400

    Enh: Добавлены классы для отображения витрины товаров

--- protected/views/index/index.php
+++ protected/views/index/index.php
@@ -1,9 +1,24 @@
 <?php
 /**
  * @var IndexController $this
+ * @var array $_data_
  */
 ?>
+<div class="wrapper">
+  <?php $this->renderPartial('_rotator', $_data_)?>
 
-<div>
-  <?php echo $this->textBlock('main_text_block')?>
+  <div class="text-container big-paddings dark-grey">
+    <?php echo $this->textBlockRegister('Текст на главной после ротатора')?>
+  </div>
+
+  <?php if($this->beginCache('index/showcase', array('duration' => 300))) { ?>
+    <?php $this->renderPartial('_showcase', array('showcase' => new Showcase(10))); ?>
+  <?php $this->endCache(); } ?>
+
+  <div class="hr1"></div>
+  <div class="text-container big-paddings dark-grey">
+    <?php echo $this->textBlockRegister('Текст на главной')?>
+  </div>
+  <div class="hr1 m20"></div>
+
 </div>
