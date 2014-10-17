737dcc309c70517a619d239f16f9399dfcd83e5d
tatarinov <tatarinov@shogo.ru>
2014-10-17 17:29:29 +0400

    Enh: Редактирование шаблонов уведомлений и е-mail'ов с разводной (backend)

--- backend/protected/modules/notification/views/notification/index.php
+++ backend/protected/modules/notification/views/notification/index.php
@@ -14,7 +14,16 @@ $this->widget('BGridView', array(
   'columns' => array(
     array('name' => 'id', 'htmlOptions' => array('class' => 'center span1'), 'filter' => false),
     array('name' => 'name', 'value' => '!empty($data->name) ? $data->name : $data->index'),
-    array('name' => 'email'),
+    array(
+      'class' => 'OnFlyEditField',
+      'name' => 'view',
+      'htmlOptions' => array('class' => 'span3'),
+      'dropDown' => CMap::mergeArray(
+        array('' => 'Не задано'),
+        $model->getViews()
+      )
+    ),
+    array('class' => 'OnFlyEditField', 'name' => 'email', 'htmlOptions' => array('class' => 'span4')),
     array('class' => 'JToggleColumn', 'name' => 'visible'),
     array('class' => 'BButtonColumn'),
   ),
