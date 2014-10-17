f3d78afc952167e849642c6a7dcc28b7fa069dc8
tatarinov <tatarinov@shogo.ru>
2014-10-17 17:51:31 +0400

    Enh: В ExRequiredValidator добавлено свойство not и работа с множеством значений

--- protected/components/validators/ExRequiredValidator.php
+++ protected/components/validators/ExRequiredValidator.php
@@ -9,6 +9,7 @@
  * Example:
  *
  * array('payer_name', 'ExRequiredValidator', 'dependedAttribute' => 'payment_id', 'dependedValue' => OrderPaymentType::BANK_PAYMENT)
+ * array('payer_name', 'ExRequiredValidator', 'dependedAttribute' => 'payment_id', 'dependedValue' => array(1, 3, 4), 'not' => true)
  */
 class ExRequiredValidator extends CRequiredValidator
 {
@@ -16,9 +17,22 @@ class ExRequiredValidator extends CRequiredValidator
 
   public $dependedValue;
 
+  public $not = false;
+
   public function validateAttribute($object, $attribute)
   {
-    if( $object->{$this->dependedAttribute} == $this->dependedValue )
+    $equal = false;
+
+    if( is_array($this->dependedValue) )
+    {
+      $equal = in_array($object->{$this->dependedAttribute}, $this->dependedValue);
+    }
+    else
+    {
+      $equal = $object->{$this->dependedAttribute} == $this->dependedValue;
+    }
+
+    if( !$this->not ? $equal : !$equal  )
     {
       parent::validateAttribute($object, $attribute);
     }
