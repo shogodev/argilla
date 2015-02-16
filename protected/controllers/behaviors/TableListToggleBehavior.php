7100a9ac303b435e53ef7299757a1f75e88f8371
tatarinov <tatarinov@shogo.ru>
2015-02-16 12:48:36 +0400

    Add: Потерянные файлы

---
+++ protected/controllers/behaviors/TableListToggleBehavior.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author Alexey Tatarivov <tatarinov@shogo.ru>
+ * @link https://github.com/shogodev/argilla/
+ * @copyright Copyright &copy; 2003-2015 Shogo
+ * @license http://argilla.ru/LICENSE
+ *
+ * Подключение:
+ * public function behaviors()
+ * {
+ *   return CMap::mergeArray(parent::behaviors(), array(
+ *     'tableListToggleBehavior' => array('class' => 'TableListToggleBehavior')
+ *   ));
+ * }
+ */
+class TableListToggleBehavior extends SBehavior
+{
+  public function isTable()
+  {
+    return !empty($_COOKIE['lineView']) ? false : true;
+  }
+}
