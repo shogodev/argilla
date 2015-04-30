bea02a6ea8e4f9ad957ae487f5b55eb2c10e1ad2
tatarinov <tatarinov@shogo.ru>
2015-04-30 10:52:22 +0400

    Enh: Незначительные обновление шаблонов

--- protected/views/_basket_header.php
+++ protected/views/_basket_header.php
@@ -4,22 +4,37 @@
  * @var Product $data
  */
 ?>
+<?php
+$this->basket->addAfterAjaxScript(new CJavaScriptExpression("
+   if( action == 'add' )
+   {
+     var parent = element.closest('#add-through-popup').data('parent');
+
+     if( parent instanceof Object )
+      image = parent.find('.animate-image');
+     else
+      image = element.closest('.product, .js-card-animate-parent').find('.animate-image');
+
+     $('.js-animate').addInCollection(image);
+   }
+  "));
+?>
 <?php $this->basket->ajaxUpdate('js-wrapper-basket-header')?>
 
 <div id="js-wrapper-basket-header">
   <?php if( !$this->basket->isEmpty() ) {?>
-    <div class="hd-basket">
-      <div class="basket-inner nofloat">
+    <div class="hd-basket js-animate">
+      <a href="<?php echo $this->createUrl('order/firstStep')?>" class="basket-inner nofloat">
         <div class="basket-caption">Корзина</div>
         <div class="basket-text">
           <span class="red"><?php echo $this->basket->countAmount()?></span> <?php echo Utils::plural($this->basket->countAmount(), 'товар,товара,товаров');?>
           <span class="red"><?php echo PriceHelper::price($this->basket->getSumTotal(), ' руб.')?></span>
         </div>
-      </div>
+      </a>
       <a href="<?php echo $this->createUrl('order/firstStep')?>" class="btn">Оформить заказ</a>
     </div>
   <?php } else {?>
-    <div class="hd-basket">
+    <div class="hd-basket js-animate">
       <div class="basket-inner nofloat">
         <div class="basket-caption">Корзина</div>
         <div class="basket-text">
--- protected/views/order/third_step.php
+++ protected/views/order/third_step.php
@@ -4,36 +4,36 @@
  * @var integer $orderId
  */
 ?>
-<div class="wrapper">
-  <?php $this->renderPartial('/_breadcrumbs');?>
-</div>
+<?php $this->renderPartial('/_breadcrumbs');?>
 
-<div class="center lightest-grey s18 pre-footer">
-  <h1 class="lightest-grey s33 uppercase m35 opensans"><?php echo Yii::app()->meta->setHeader('Ваш заказ принят');?></h1>
+<?php $phonesText = array();?>
+<?php foreach(ViewHelper::phones() as $phone) {?>
+  <?php $phonesText[] = '<p class="m45" style="font-size:44px;margin-top:15px"><span class="blue">'.$phone->value.'</span> '.$phone->description.'</p>';?>
+<?php }?>
 
-  <?php $phonesText = ''?>
-  <?php if( $this->getHeaderContacts() && $phones = $this->getHeaderContacts()->getFields('phones') ) {?>
-    <?php $phonesText = '<div class="m10">Вы можете задать вопрос по телефонам:</div><div class="m20">'?>
-    <?php if( isset($phones[0]) ) $phonesText .= '<div class="m5"><a class="phone-number opensans s25 bb red" href="tel:'.$phones[0]->getClearPhone().'">'.$phones[0]->value.$phones[0]->description.'</a></div>';?>
-    <?php if( isset($phones[1]) ) $phonesText .= '<div class="m5"><a class="phone-number opensans s25 bb red" href="tel:'.$phones[1]->getClearPhone().'">'.$phones[1]->value.$phones[1]->description.'</a></div>';?>
-    <?php $phonesText .= '</div>'?>
-  <?php }?>
+<?php $defaultText = '<div class="container text-container m35 center">
+  <p class="s23 upcase m15">Ваш заказ принят</p>
+  <p class="m20 s17">
+    Ваш заказ был успешно принят и находится в обработке. <br>
+    Номер Вашего заказа: {orderId}<br>
+    В ближайшее время менеджер свяжется с Вами для уточнения <br>
+    необходимых деталей. <br>
+    После этого вы сможете оплатить заказ. <br>
+    Благодарим Вас за совершение покупок на {projectName}<br>
+  </p>
+  {phones}
+  <p><a href="{mainUrl}" class="medium-btn upcase s20 green-btn">на главную</a></p>
+</div>';?>
 
-  <?php $defaultText = '<div class="m3">Ваш заказ был успешно принят и находится в обработке.</div>
-    <div class="s24 bb white m20 opensans">Номер Вашего заказа: <span class="red">{orderId}</span></div>
-    <div class="m70">
-      В ближайшее время менеджер свяжется с Вами для уточнения необходимых деталей.<br />
-      После этого Вы сможете оплатить заказ.<br />
-      Благодарим Вас за совершение покупок на motoremont.ru
-    </div>
-    {phones}
-    <a class="btn red-contour-btn solid-btn rounded-btn white-body-btn h34btn p10btn opensans s15 bb uppercase" href="{mainUrl}">Вернуться на главную</a>';
-  ?>
+<?php echo $this->textBlockRegister('Заказ принят', $defaultText, null, array(
+  '{projectName}' => Yii::app()->params->project,
+  '{orderId}' => $orderId,
+  '{mainUrl}' => $this->createUrl('index/index'),
+  '{phones}' => count($phonesText) > 0 ? '<p class="s17">Вы можете задать вопрос по '.Utils::plural(count($phonesText), 'телефону|телефонам').':</p>'.implode('', $phonesText) : '',
+))?>
 
-  <?php echo $this->textBlockRegister('Заказ принят', $defaultText, null, array(
-    '{orderId}' => $orderId,
-    '{mainUrl}' => $this->createUrl('index/index'),
-    '{phones}' => $phonesText,
-  ))?>
-
-</div>
+<script>
+  // <![CDATA[
+  $('#structure').css('background', 'url(/i/basket-3.jpg) 50% 160px no-repeat');
+  // ]]>
+</script>
