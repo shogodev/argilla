41012b83d9b4312f448942fb7e1227c13bf952fe
glagolev <glagolev@shogo.ru>
2015-06-04 15:00:07 +0300

    Enh: В продакшене теперь подключается yiilite

--- index.php
+++ index.php
@@ -5,16 +5,18 @@ if( file_exists($production) )
 {
   error_reporting(0);
   ini_set('display_errors', 0);
+  $yii = 'yiilite.php';
   $config = require_once $production;
 }
 else
 {
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
+  $yii = 'yii.php';
   $config = require_once __DIR__.'/protected/config/frontend.php';
 }
 
-require_once(realpath(__DIR__.'/../yii/framework/yii.php'));
+require_once(realpath(__DIR__.'/../yii/framework/'.$yii));
 require_once('protected/components/FApplication.php');
 
 $app = new FApplication($config);
