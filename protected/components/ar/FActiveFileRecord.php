f3ed769e04d73845dde8d58695b34b748762b817
tatarinov <tatarinov@shogo.ru>
2015-10-20 15:48:54 +0400

    Enh: Доработка загрузки изображений во frontend

--- protected/components/ar/FActiveFileRecord.php
+++ protected/components/ar/FActiveFileRecord.php
@@ -1,25 +1,38 @@
 <?php
 /**
- * @author Sergey Glagolev <glagolev@shogo.ru>
+ * @author Sergey Glagolev <glagolev@shogo.ru>, Alexey Tatarinov <tatarinov@shogo.ru>
  * @link https://github.com/shogodev/argilla/
  * @copyright Copyright &copy; 2003-2014 Shogo
  * @license http://argilla.ru/LICENSE
  * @package frontend.components.ar
  */
+
+/**
+ * Class FActiveFileRecord
+ *
+ * @var string $uploadPath - путь для загрузки файлов, по умолчанию /f/названте_модели
+ * @var string $fileModel - модель хранения файлов, по умолчанию унаследованая от FActiveFileRecord
+ * @var string $formAttribute - поле модели храняшие имя файла
+ */
 class FActiveFileRecord extends FActiveRecord
 {
   public $maxFiles = 5;
 
-  public $fileTypes = array('zip', 'rar', 'doc', 'docx', 'pdf', 'jpg');
+  public $fileTypes = array('zip', 'rar', 'doc', 'docx', 'pdf', 'jpg', 'jpeg', 'png', 'gif');
 
   public $uploadPath;
 
   public $formAttribute = 'file';
 
+  /**
+   * @var $fileModel - модель хранения файлов
+   */
+  public $fileModel;
+
   public function init()
   {
     if( empty($this->fileModel) )
-      throw new CHttpException(500, 'Set fileModel property');
+      $this->fileModel = get_class($this);
 
     $scenario = Yii::app()->request->isPostRequest ? 'upload' : $this->getScenario();
     $this->setScenario($scenario);
@@ -50,8 +63,8 @@ class FActiveFileRecord extends FActiveRecord
 
   protected function initPath()
   {
-    if( !$this->uploadPath )
-      $this->uploadPath = Yii::getPathOfAlias('webroot').'/f/'.strtolower(get_called_class()).'/';
+    $this->uploadPath = Yii::getPathOfAlias('webroot').'/f/'.($this->uploadPath ? $this->uploadPath : strtolower(get_called_class()));
+    $this->uploadPath = preg_replace('/\/$/', '', $this->uploadPath).'/';
 
     if( !file_exists($this->uploadPath) )
     {
--- protected/widgets/FMultiFileUpload.php
+++ protected/widgets/FMultiFileUpload.php
@@ -1,17 +1,38 @@
 <?php
 /**
- * @author Sergey Glagolev <glagolev@shogo.ru>
+ * @author Sergey Glagolev <glagolev@shogo.ru>, Alexey Tatarinov <tatarinov@shogo.ru>
  * @link https://github.com/shogodev/argilla/
  * @copyright Copyright &copy; 2003-2014 Shogo
  * @license http://argilla.ru/LICENSE
  * @package frontend.widgets
+ * Пример исполбзования в форме
+ * 'img' => array(
+ *   'type' => 'FMultiFileUpload',
+ *   'form' => $this,
+ *  ),
+ */
+/**
+ * Class FMultiFileUpload
+ * @var FActiveFileRecord $model
  */
 class FMultiFileUpload extends CMultiFileUpload
 {
+  /**
+   * @var FForm
+   */
   public $form;
 
   public function init()
   {
+    if( !($this->model instanceof FActiveFileRecord) )
+      throw new CHttpException(500, 'Модель '.get_class($this->model).' должна быть потомком FActiveFileRecord');
+
+    if( !($this->form instanceof FForm) )
+      throw new CHttpException(500, 'Атрибут form должен ссылатся на родительскую форму');
+
+    if( $this->form->ajaxSubmit )
+      throw new CHttpException(500, 'Необходимо выключить своство ajaxSubmit в форме '.get_class($this->form));
+
     if( !isset($this->accept) )
       $this->accept = implode('|', $this->model->fileTypes);
 
@@ -26,4 +47,6 @@ class FMultiFileUpload extends CMultiFileUpload
     if( !isset($this->htmlOptions['size']) )
       $this->htmlOptions['size'] = 1;
   }
+
+
 }
