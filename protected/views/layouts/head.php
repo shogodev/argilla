2f044df45fb1df4aff622b5dc3d33057cc7e4d76
rubanov <rubanov@shogo.ru>
2016-04-13 16:08:37 +0300

    Метатег для нормального вьюпорта на мобильных

--- protected/views/layouts/head.php
+++ protected/views/layouts/head.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8" />
   <title><?php echo Yii::app()->meta->title;?></title>
   <?php echo Yii::app()->meta->custom;?>
-  <meta name="viewport" content="target-densitydpi=device-dpi" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <base href="<?php echo Yii::app()->homeUrl?>" />
 
   <?php echo CHtml::linkTag("canonical", null, Yii::app()->controller->getCanonicalUrl());?>
