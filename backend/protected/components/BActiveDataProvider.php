51f833e8b88bb73ab558e1a13776f44329f1b925
ostashev <ostashev@shogo.ru>
2016-06-09 16:58:27 +0300

    Вынес выбор количества элементов на странице из BActiveDataProvider в BPagination

--- backend/protected/components/BActiveDataProvider.php
+++ backend/protected/components/BActiveDataProvider.php
@@ -8,13 +8,13 @@
  * <code>
  *  // инициализация датапровайдера
  *  $dataProvider = new BActiveDataProvider('BNews', array('sort' => array('defaultOrder' => 'date DESC')));
- *  $dataProvider->setPageSizeElements(10000000 => 'Все', 5 => 5, 10 => 10, 25 => 25);
+ *  $dataProvider->pagination->setPageSizeElements(10000000 => 'Все', 5 => 5, 10 => 10, 25 => 25);
  * </code>
  *
- * Для того чтобы получить форму необходимо вызвать BActiveDataProvider::getPageSizeForm()
+ * Для того чтобы получить форму необходимо вызвать BActiveDataProvider::getPagination()->getPageSizeForm()
  * @example
  * <code>
- *  $dataProvider->getPageSizeForm();
+ *  $dataProvider->pagination->getPageSizeForm();
  * </code>
  * При этом произойдет отобращений как самой формы, так и скрипта, который перехватывает эвэнт onChange поля формы
  * и автоматически сабмитит.
@@ -24,170 +24,19 @@
  * @copyright Copyright &copy; 2003-2014 Shogo
  * @license http://argilla.ru/LICENSE
  * @package backend.components
+ *
+ *
+ * @property BPagination $pagination
  */
 class BActiveDataProvider extends CActiveDataProvider
 {
-  const PAGINATION_PAGE_SIZE = 10;
-
-  const MAX_PAGE_SIZE = PHP_INT_MAX;
-
-  /**
-   * Хранит в себе строку тэгов формы с заданными параметрами
-   *
-   * @var string of html tags with form
-   */
-  protected $pageSizeForm;
-
-  /**
-   * Массив элементов, используемых для вывода
-   * в форме вариантов отображения элементов страниц
-   *
-   * @var array
-   */
-  protected $pageSizeFormElements = array(10 => 10, 50 => 50, 100 => 100, 500 => 500, 1000 => 1000, 5000 => 5000, 'all' => 'Все');
-
-  /**
-   * Имя переменной для передачи и перехвата в $_GET запросе
-   *
-   * @var staring
-   */
-  protected $attributeName;
-
-  /**
-   * Текущий размер вывода элементов на страницу
-   *
-   * @var int
-   */
-  protected $currentPageSize = self::PAGINATION_PAGE_SIZE;
-
-  /**
-   * @param mixed $modelClass
-   * @param array $config
-   */
-  public function __construct($modelClass, array $config = array())
+  public function __construct($modelClass, $config = array())
   {
     parent::__construct($modelClass, $config);
-
-    $this->attributeName = $this->getId() . '_count';
-
-    if( $this->pagination !== false )
-      $this->pagination = array('pageSize' => $this->createPageSize());
-  }
-
-  /**
-   * Возвращает текущее значение количества элементов на страницу
-   *
-   * @return int
-   */
-  public function getCurrentPageSize()
-  {
-    return $this->currentPageSize;
-  }
-
-  /**
-   * Присваивание новых вариантов значений количества элементов
-   *
-   * @param array $elements
-   */
-  public function setPageSizeElements(array $elements)
-  {
-    $this->pageSizeFormElements = $elements;
-  }
-
-  /**
-   * Получение текущего значения вывода количества элементов на страницу
-   *
-   * @return int
-   */
-  public function getPageSizeFormElements()
-  {
-    if( !empty($this->pageSizeFormElements['all']) )
-    {
-      unset($this->pageSizeFormElements['all']);
-      $this->pageSizeFormElements[self::MAX_PAGE_SIZE] = 'Все';
-    }
-
-    return $this->pageSizeFormElements;
-  }
-
-  /**
-   * Создание формы для отображения вариантов количества элементов
-   *
-   * @return string of form
-   */
-  public function getPageSizeForm()
-  {
-    if( empty($this->pageSizeForm) )
-      $this->createPageSizeForm();
-
-    return $this->pageSizeForm;
-  }
-
-  /**
-   * Создание формы, включающей в себя <select>-поле с вариантом отображения количества элементов
-   * Так же создания скрипта для обработки onChange эвэнтов изменения <select>
-   */
-  private function createPageSizeForm()
-  {
-    $formProperties = array('class' => 'page-size');
-
-    $form  = CHtml::beginForm($this->getClearUrl(), 'get', $formProperties);
-    $form .= CHtml::openTag('span', array('class' => 'page-size-title'));
-    $form .= 'Отображать по: ';
-    $form .= CHtml::closeTag('span');
-    $form .= CHtml::dropDownList($this->attributeName, $this->currentPageSize, $this->getPageSizeFormElements());
-    $form .= CHtml::endForm();
-
-    $this->pageSizeForm = $form;
-
-    $formChangeHandler = <<<EOD
-  $('body').on('change', '.{$formProperties['class']} select', function(){
-    if( window.History.enabled )
-    {
-      var url = $(this).parents('form').attr('action').split('?');
-      var params = url[1] === undefined ? [] : $.deparam.querystring('?'+url[1]);
-      params[$(this).attr('id')] = $(this).val();
-      window.History.pushState(null, document.title, decodeURIComponent($.param.querystring(url[0], params)));
-    }
-    else
-    {
-      $(this).parents('.{$formProperties['class']}').submit();
-    }
-  });
-EOD;
-
-    Yii::app()->getClientScript()->registerScript('pageSizeChangeHandler', $formChangeHandler, CClientScript::POS_READY);
-  }
-
-  /**
-   * Создания количества элементов на страницу
-   * Если установлен $_GET, содержащий $this->attributeName, то значение меняется со стандартного на новое
-   *
-   * @return int
-   */
-  private function createPageSize()
-  {
-    if( !empty($_GET[$this->getId() . '_count']) )
-      $this->currentPageSize = $_GET[$this->attributeName];
-
-    if( $this->currentPageSize === 'all' )
-      $this->currentPageSize = self::MAX_PAGE_SIZE;
-
-    return $this->currentPageSize;
-  }
-
-  /**
-   * Получение чистого адреса для сабмита формы
-   * Убирает текущий $_GET из запроса, сохраняя остальные параметры
-   *
-   * @return string
-   */
-  private function getClearUrl()
-  {
-    $params = $_GET;
-
-    unset($params[$this->attributeName]);
-    unset($params['ajax']);
-    return Yii::app()->createUrl(Yii::app()->controller->uniqueId.'/'.Yii::app()->controller->getAction()->getId(), $params);
+    $this->setPagination([
+      'class' => 'BPagination',
+      'pageVar' => $this->getId().'_page',
+      'pageSizeVar' => $this->getId().'_count',
+    ]);
   }
 }
