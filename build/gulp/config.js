678844268b62f657963fe0e35b1090f89e7c3159
rubanov <rubanov@shogo.ru>
2017-11-15 09:28:33 +0300

    js paths for gulp

--- build/gulp/config.js
+++ build/gulp/config.js
@@ -5,29 +5,24 @@ var path = require('path');
 var rootPath = path.join(__dirname, '../../');
 var sassTempDir = '~sasstemp_' + new Date().getTime();
 
+var vendorScripts = [
+  rootPath + 'js/src/vendor/jquery.js',
+  rootPath + 'js/src/vendor/jquery-ui.js',
+  rootPath + 'js/src/vendor/hammer.js',
+  rootPath + 'js/src/vendor/jquery_plugins/*.js',
+  rootPath + 'js/src/vendor/jquery_ui_widgets/*.js',
+  rootPath + 'js/src/vendor/**/*.js'
+]
+
 module.exports = {
   rootPath: rootPath,
 
   js: {
     dest: rootPath + 'js',
-    src: argv['vendor'] ? [
-      rootPath + 'js/src/vendor/jquery.js',
-      rootPath + 'js/src/vendor/jquery-ui.js',
-      rootPath + 'js/src/vendor/hammer.js',
-      rootPath + 'js/src/vendor/jquery_plugins/*.js',
-      rootPath + 'js/src/vendor/jquery_ui_widgets/*.js',
-      rootPath + 'js/src/vendor/**/*.js'
-    ] : [
+    src: argv['vendor'] ? vendorScripts : [
       rootPath + 'js/src/common.js'
     ],
-    watchSrc: argv['vendor'] ? [
-      rootPath + 'js/src/vendor/jquery.js',
-      rootPath + 'js/src/vendor/jquery-ui.js',
-      rootPath + 'js/src/vendor/hammer.js',
-      rootPath + 'js/src/vendor/jquery_plugins/*.js',
-      rootPath + 'js/src/vendor/jquery_ui_widgets/*.js',
-      rootPath + 'js/src/vendor/**/*.js'
-    ] : [
+    watchSrc: argv['vendor'] ? vendorScripts : [
       rootPath + 'js/src/common.js',
       rootPath + 'js/src/modules/*.js'
     ],
